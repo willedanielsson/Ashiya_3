c2ed910dc5110ba4c41ca4ee1f9bb431f0db4605
William Danielsson <William@williams-mbp.lan>
2014-12-26 10:11:36 +0100

    Initial

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="animate.css" />
+	<link href='http://fonts.googleapis.com/css?family=Chivo:900' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Francois+One' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<script src="jquery-2.js"></script>
+	<title>Ashiya</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<header>
+		<div class="nav-logo">
+			<img src="Ashiya.png" class="nav-ashiya">
+		</div>	
+
+	</header>
---
+++ index.php
@@ -0,0 +1,83 @@
+	
+	<?php include ("header.php"); ?>
+
+		<div class="content">
+			<div id="information">
+				<div class="content_info" id="style">
+					<h1>ENKELT MEN STILFULLT</h1>
+					<h2> </h2>
+						<div class="slideText">
+							<p>Besökare upplever en simpel och luftigt designad hemsida
+							som mer professionell och attraktiv.
+							Ashiya jobbar hårt för att utforma en webdesign som inte
+							innehåller för mycket onödig information och upplevs krånglig. </p>
+						</div>	
+				</div>
+
+				<div class="content_info" id="seo">
+					<h1>Search Engine Optimization</h1>
+					<h2></h2>
+					<div class="slideText">
+						<p>Att hamna högt upp hos sökmotorer som Google har blivit
+						allt viktigare. Ashiyas webdesign får dig högre upp på
+						listan tack vare att vi använder de senaste språken av
+						webdesign samt att vi lägger ner extra mycket kraft att
+						få din sina att bli populär bland sökord som gynnar dig!</p>
+					</div>	
+
+				</div>	
+
+				<div class="content_info" id?="responsive">
+					<h1>RESPONSIV WEBDESIGN</h1>
+					<h2> </h2>
+					<div class="slideText">
+						<p>Idag är det inte bara datorer som används för att surfa
+						på nätet. Mobiler och plattor har blivit allt vanligare
+						vilket kan komplicera användarvänligheten hos en sida som
+						bara har en webdesign utformad efter datorer. Vi ser till att
+						din sida ser bra ut trots vilken upplösning besökare har,
+						testa denna sida genom att minska webläsarfönstret!</p>
+					</div>	
+				</div>		
+			</div>
+
+
+			<div id="contact">
+				<h1>Kontakt</h1>
+
+				<div class="nav-social">
+
+					<ul>
+						<li> 
+							<a href="https://www.facebook.com/AshiyaDesign">
+								<span class="nav-icon">f</span>
+							</a>
+						</li>
+
+						<li class=""> 
+							<a href="https://twitter.com/WilleDanielsson">
+								<span class="nav-icon">t</span>
+							</a>
+						</li>
+
+						<li class=""> 
+							<a href="http://www.linkedin.com/profile/view?id=217530745&trk=hb_tab_pro_top">
+								<span class="nav-icon">l</span>
+							</a>
+						</li>
+					</ul>	
+				</div>
+				<div id="phone">
+					<span id="icon" class="fa fa-mobile"></span>
+					<span>+46704 - 86 91 59</span>
+				</div>
+
+				<div id="mail">
+					<span class="fa fa-envelope"></span>
+					<span>william.l.danielsson(at)gmail.com</span>
+				</div>
+
+			</div>
+		</div>
+
+<?php include ("footer.php"); ?>
